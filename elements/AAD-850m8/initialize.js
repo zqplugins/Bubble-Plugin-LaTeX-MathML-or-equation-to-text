0f3a970807f546ec3a72bd05038bd8cb89cd2268
zqplugins <137153217+zqplugins@users.noreply.github.com>
2023-09-07 12:38:24 +0300

    Automatic commit from Bubble

---
+++ elements/AAD-850m8/initialize.js
@@ -0,0 +1,10 @@
+function(instance, context) {
+let mapped=`
+            <div id="MathDiv">
+        <span id="math"></span><br>
+        <textarea id="input" cols="50" rows="20">This is a test: $$Re{z} =\frac{npi dfrac{	heta +psi}{2}}{
+            left(dfrac{	heta +psi}{2}\right)^2 + left( dfrac{1}{2}
+            log leftlvertdfrac{B}{A}\right\rvert\right)^2}$$</textarea>
+                </div>
+          `;instance.canvas.append(mapped),function(t,a){(a=t.createElement("script")).type="text/javascript",a.async=!0,a.onload=function(){},a.src="https://cdn.mathjax.org/mathjax/latest/MathJax.js",t.getElementsByTagName("head")[0].appendChild(a)}(document);
+}
