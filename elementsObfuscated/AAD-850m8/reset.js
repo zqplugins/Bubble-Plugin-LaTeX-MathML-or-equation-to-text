7fb50c99e720a5bfe7d6322077618bae312a0e4d
Андрій Пічкорський <plugins@zeroqode.com>
2023-11-06 11:36:51 +0200

    Update element "LaTeX & MathML", added action "Math to formula"

---
+++ elementsObfuscated/AAD-850m8/reset.js
@@ -0,0 +1,4 @@
+function(instance, context) {
+	function reset(_0x495691,_0x26de1a){}
+	return reset(instance, context);
+}
---
+++ elementsObfuscated/AAE-850ma/reset.js
@@ -0,0 +1,4 @@
+function(instance, context) {
+	function reset(_0x36ab01,_0x147d2c){}
+	return reset(instance, context);
+}
